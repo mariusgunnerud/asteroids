aa4c83408d615a0b123b20352e5a4540b6b1ce95
MrMagu <mariusgunnerud@gmail.com>
2025-04-02 06:40:28 -0400

    added time.Clock() and .tick()

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#pygame documentation https://www.pygame.org/docs/ref/pygame.html
+#Useful for this project: pygame documentation https://www.pygame.org/docs/ref/pygame.html
 
 import pygame
 import constants
@@ -8,6 +8,8 @@ import constants
 def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,7 @@ def main():
                 return
             
         screen.fill("black", rect=None, special_flags=0)
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
 
 if __name__ == "__main__":
