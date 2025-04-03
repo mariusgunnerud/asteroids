80cfec58cbdcef6e8d4a715929ad2f9f2c499dc8
MrMagu <mariusgunnerud@gmail.com>
2025-04-03 15:06:28 -0400

    added player triangle

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 
 #Useful for this project: pygame documentation https://www.pygame.org/docs/ref/pygame.html
 
-import pygame
-import constants
+from pygame import *
+from constants import *
+from player import *
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -17,6 +19,8 @@ def main():
                 return
             
         screen.fill("black", rect=None, special_flags=0)
+        player.draw(screen)
+
         dt = clock.tick(60) / 1000
         pygame.display.flip()
 
